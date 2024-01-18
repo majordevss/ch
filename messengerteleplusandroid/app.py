e8c17afd0f90a41bba57c291fec5766f4cbd5749
majordevs2023 <majordevs2023@gmail.com>
2024-01-18 22:08:26 +0300

    uddat

---
+++ messengerteleplusandroid/app.py
@@ -0,0 +1,23 @@
+import firebase_admin
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {"databaseURL": "https://plus-messegner-default-rtdb.asia-southeast1.firebasedatabase.app/"})
+ref = db.reference("app")
+
+# Write the data to the database
+
+import json
+
+# Step 1: Read JSON data from file
+with open('config.json', 'r') as json_file:
+    json_data = json_file.read()
+
+# Step 2: Parse JSON data
+try:
+    parsed_data = json.loads(json_data)
+    print(parsed_data)
+    ref.update(parsed_data) 
+except json.JSONDecodeError as e:
+    print("Error decoding JSON:", e)
+
